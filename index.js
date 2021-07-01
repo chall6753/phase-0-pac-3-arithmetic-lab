8de9c40343a52f2053164f5e39314b4472bd956c
Charles <chall70101@gmail.com>
2021-07-01 07:48:26 -0600

    completed assignment

---
+++ index.js
@@ -0,0 +1,52 @@
+
+function add(){
+1+3
+}
+
+function subtract(){
+    6-1   
+}
+
+function multiply(){
+ 6*3      
+}
+
+function divide(){
+    5/3
+}
+
+function add(a, b){
+    return a+b
+}
+
+function subtract(a, b){
+return a-b
+    }
+
+function multiply(a, b){
+    return a*b
+    }
+
+function divide(a, b){
+        return a/b
+        }
+
+function increment(n){
+return n += 1
+
+}
+
+function decrement(n){
+    return n -= 1
+    
+    }
+
+function makeInt(n){
+    return parseInt(n,10)
+        
+    }
+
+function preserveDecimal(n){
+    return parseFloat(n,10)
+    
+    }
